931fb1f2a9c992342041409b5687ace5e243dd03
lutfialquraan <lutfi@gmx.com>
2022-02-02 00:15:14 +0200

    calculate checksum while reading the file

--- src/client/TcpClient.java
+++ src/client/TcpClient.java
@@ -8,7 +8,9 @@ import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.math.BigInteger;
 import java.net.Socket;
+import java.security.MessageDigest;
 
 /**
  * <h1>
@@ -42,13 +44,27 @@ public class TcpClient {
             socketDataOutputStream.writeLong(checksum);
             socketDataOutputStream.flush();
             System.out.println("The checksum value for the file (" + name + ") = " + checksum);
+            MessageDigest md = MessageDigest.getInstance("MD5");
 
+            long size = file.length();
+            socketDataOutputStream.writeLong(size);
+            socketDataOutputStream.flush();
             // write the file to the socket
             byte[] buffer = new byte[Constants.BUFFER_SIZE];
             while (bufferedInputStream.read(buffer, 0, buffer.length) != -1) {
+                md.update(buffer, 0, buffer.length);
                 socketDataOutputStream.write(buffer, 0, buffer.length);
             }
+            //Hash value array
+            byte[] digest = md.digest();
+            //1 indicates that this is an unsigned integer
+            BigInteger bigInteger = new BigInteger(1, digest);
+            //Output in hexadecimal form
+            System.out.println(bigInteger.toString(16));
             socketDataOutputStream.flush();
+            socketDataOutputStream.writeUTF(bigInteger.toString(16));
+            socketDataOutputStream.flush();
+
             System.out.println("The file (" + name + ") has sent to the server with " + file.length() + " byte size");
         } catch (Exception e) {
             e.printStackTrace();
--- src/server/ServerProcess.java
+++ src/server/ServerProcess.java
@@ -8,8 +8,12 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * <h1>
@@ -37,10 +41,13 @@ class ServerProcess {
             System.out.println("Checksum: " + checksum);
             System.out.println("==================================");
 
+            long size = socketDataInputStream.readLong();
+
 
             File uploadedFile = new File(Constant.FOLDER_NAME + FileSystems.getDefault().getSeparator() + fileName);
-            writeFile(socketDataInputStream, uploadedFile);
-
+            writeFile(socketDataInputStream, uploadedFile, size);
+            String md5 = socketDataInputStream.readUTF();
+            System.out.println("Client MD5 = " + md5);
             System.out.println("Your " + fileName + " have been downloaded successfully on server ...");
             long calculatedCrc = Utilities.calculateCrc(uploadedFile);
         } catch (Exception e) {
@@ -49,17 +56,27 @@ class ServerProcess {
     }
 
     private void writeFile(DataInputStream socketBufferedInputStream,
-                           File uploadedFile) throws IOException {
+                           File uploadedFile, long size) throws IOException, NoSuchAlgorithmException {
         if (!uploadedFile.getParentFile().exists()) {
             uploadedFile.getParentFile().mkdirs();
         }
+        int currentSize = 0;
         try (FileOutputStream fileOutputStream = new FileOutputStream(uploadedFile);
              BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)
         ) {
+            MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] buffer = new byte[Constant.BUFFER_SIZE];
-            while (socketBufferedInputStream.read(buffer, 0, buffer.length) != -1) {
+            while (currentSize < size) {
+                currentSize += socketBufferedInputStream.read(buffer, 0, buffer.length);
+                md.update(buffer, 0, buffer.length);
                 bufferedOutputStream.write(buffer, 0, buffer.length);
             }
+            //Hash value array
+            byte[] digest = md.digest();
+            //1 indicates that this is an unsigned integer
+            BigInteger bigInteger = new BigInteger(1, digest);
+            //Output in hexadecimal form
+            System.out.println(bigInteger.toString(16));
         }
     }
 
